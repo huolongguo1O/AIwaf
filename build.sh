0042c7ab23960af83dce686ec52b76ea970f91b9
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-08-10 20:44:02 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 python -m build
+cd dist
+pip install check-0.3.5-py3-none-any.whl
+cd ..
+
 git clone https://github.com/nginx/nginx
 cd nginx
 make clean
 sudo /usr/local/nginx/sbin/nginx -s stop 
-./auto/configure --add-module=../
+echo input your nginx compile flags and press enter:
+./auto/configure --add-module=../ `head -n 1`
 make&&sudo make install 
 sudo /usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
-cat /usr/local/nginx/logs/error.log|tail -n 10 
+
